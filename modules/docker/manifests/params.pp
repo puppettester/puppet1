85886a9e33fc5c1d897af77bd090f8e6b9711437
puppettester <ramakrishnan_vee@yahoo.co.in>
2017-12-07 12:22:23 +0530

    changed the key for Docker Package Repository

--- modules/docker/manifests/params.pp
+++ modules/docker/manifests/params.pp
@@ -137,9 +137,7 @@ class docker::params {
       $package_cs_source_location = 'http://packages.docker.com/1.9/apt/repo'
       $package_cs_key_source = 'http://packages.docker.com/1.9/apt/gpg'
       $package_cs_key = '0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e'
-      $package_source_location = 'http://apt.dockerproject.org/repo'
-      $package_key_source = 'http://apt.dockerproject.org/gpg'
-      $package_key = '58118E89F3A912897C070ADBF76221572C52609D'
+      $package_key = '9DC858229FC7DD38854AE2D88D81803C0EBFCD88'
 
       if ($::operatingsystem == 'Debian' and versioncmp($::operatingsystemmajrelease, '8') >= 0) or
         ($::operatingsystem == 'Ubuntu' and versioncmp($::operatingsystemrelease, '15.04') >= 0) {
